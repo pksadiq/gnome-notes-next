c2809c23abfd54498f6061174a533f1a50d22a97
Mohammed Sadiq <sadiq@sadiqpk.org>
2018-09-20 07:23:01 +0530

    tests: Add settings tests

---
+++ tests/settings.c
@@ -0,0 +1,109 @@
+/* settings.c
+ *
+ * Copyright 2018 Mohammed Sadiq <sadiq@sadiqpk.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include <glib.h>
+
+#include "gn-settings.h"
+
+static void
+test_settings_geometry (void)
+{
+  g_autoptr(GnSettings) settings = NULL;
+  GdkRectangle geometry = {100, 200, 300, 400};
+  GdkRectangle reset = {0, 0, 0, 0};
+  GdkRectangle out;
+  gboolean is_maximized;
+
+  settings = gn_settings_new ("org.sadiqpk.notes");
+  g_assert (g_type_is_a (GN_TYPE_SETTINGS, G_TYPE_SETTINGS));
+  g_assert (GN_IS_SETTINGS (settings));
+
+  gn_settings_set_window_maximized (settings, 0);
+  g_assert_false (gn_settings_get_window_maximized (settings));
+
+  gn_settings_set_window_maximized (settings, 1);
+  g_assert_true (gn_settings_get_window_maximized (settings));
+
+  /*
+   * gbooleans are typedef to gint.  So test if non boolean
+   * values are clamped.
+   */
+  gn_settings_set_window_maximized (settings, 100);
+  is_maximized = gn_settings_get_window_maximized (settings);
+  g_assert_cmpint (is_maximized, ==, 1);
+
+  gn_settings_set_window_geometry (settings, &geometry);
+  gn_settings_get_window_geometry (settings, &out);
+  g_assert_cmpint (out.x, ==, geometry.x);
+  g_assert_cmpint (out.y, ==, geometry.y);
+  g_assert_cmpint (out.width, ==, geometry.width);
+  g_assert_cmpint (out.height, ==, geometry.height);
+  out = reset;
+
+  /* Save the settings, create a new object, and check again */
+  g_object_unref (settings);
+  settings = gn_settings_new ("org.sadiqpk.notes");
+  g_assert (GN_IS_SETTINGS (settings));
+
+  is_maximized = gn_settings_get_window_maximized (settings);
+  g_assert_cmpint (is_maximized, ==, 1);
+
+  gn_settings_get_window_geometry (settings, &out);
+  g_assert_cmpint (out.x, ==, geometry.x);
+  g_assert_cmpint (out.y, ==, geometry.y);
+  g_assert_cmpint (out.width, ==, geometry.width);
+  g_assert_cmpint (out.height, ==, geometry.height);
+}
+
+static void
+test_settings_first_run (void)
+{
+  g_autoptr(GnSettings) settings = NULL;
+  gboolean is_first_run;
+
+  settings = gn_settings_new ("org.sadiqpk.notes");
+  g_assert (GN_IS_SETTINGS (settings));
+
+  g_assert_true (gn_settings_get_is_first_run (settings));
+
+  /* Save the settings, create a new object, and check again */
+  g_object_unref (settings);
+  settings = gn_settings_new ("org.sadiqpk.notes");
+  g_assert (GN_IS_SETTINGS (settings));
+
+  g_assert_false (gn_settings_get_is_first_run (settings));
+}
+
+int
+main (int   argc,
+      char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  /*
+   * "/first_run/settings" should be the first test as it is supposed
+   * to be changing only on the first run.  Changing this order will
+   * result in test failure.
+   */
+  g_test_add_func ("/settings/first_run", test_settings_first_run);
+  g_test_add_func ("/settings/geometry", test_settings_geometry);
+
+  return g_test_run ();
+}
